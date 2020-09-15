642ff0c736d2357949731666f753440f228d8720
adya27 <31897074+adya27@users.noreply.github.com>
2020-09-15 09:14:56 +0300

    task-01

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+let listItem = document.querySelectorAll("li.item");
+console.log(`В списке ${listItem.length} категории.`);
+listItem.forEach((element) => {
+  console.log(
+    `\u2022 Категория: ${
+      element.querySelector("h2").textContent
+    }\n\u2022 Количество: ${element.querySelectorAll("li").length}`
+  );
+});
