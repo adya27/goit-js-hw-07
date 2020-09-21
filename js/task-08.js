5b058fc309fb5cf6177c292a833885df4cda452f
adya27 <31897074+adya27@users.noreply.github.com>
2020-09-21 22:39:23 +0300

    добавил стили

--- js/task-08.js
+++ js/task-08.js
@@ -6,13 +6,13 @@ const inputItem = document.querySelector("#controls input");
 const inputItemBoxes = document.querySelector("#boxes");
 let string = "";
 
-inputItem.addEventListener("change", onInputChange);
+inputItem.addEventListener("blur", onInputChange);
 createButton.addEventListener("click", createBoxes);
 destroyButton.addEventListener("click", destroyBoxes);
 
 function onInputChange(event) {
   let amount = event.currentTarget.value;
-  console.log("onInputChange -> amount", amount);
+  //   console.log("onInputChange -> amount", amount);
 
   for (let i = 0; i < amount; i += 1) {
     string += `<div
@@ -26,7 +26,7 @@ function onInputChange(event) {
               ${Math.round(Math.random() * (256 - 1) + 1)}, 
               ${Math.round(Math.random() * (256 - 1) + 1)});"></div>`;
   }
-  console.log("onInputChange -> string", string);
+  //   console.log("onInputChange -> string", string);
   return string;
 }
 
